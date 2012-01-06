2d2e1d39da835948e03aaebad6abaf67563676b2
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2012-01-06 15:50:01 +0100

    SQ - one wrap succeeds

--- src/main/java/com/coremedia/codekata/wordwrap/SimpleWordWrapper.java
+++ src/main/java/com/coremedia/codekata/wordwrap/SimpleWordWrapper.java
@@ -6,6 +6,32 @@ package com.coremedia.codekata.wordwrap;
 public class SimpleWordWrapper implements WordWrapper {
 
   public String wrap(String lineToWrap, int maxCharsPerLine) {
-    return lineToWrap;
+    return (lineToWrap.length() <= maxCharsPerLine)
+      ? lineToWrap
+      : wrapLine(lineToWrap, maxCharsPerLine);
+  }
+
+  private String wrapLine(String lineToWrap, int maxCharsPerLine) {
+    StringBuilder newLineBuilder = new StringBuilder();
+
+    String[] words = lineToWrap.split(" ");
+
+    int lineLength = 0;
+    for (String word : words) {
+      String wordWithTrainlingSpace = word + " ";
+      boolean spaceLeftInLine = (lineLength + wordWithTrainlingSpace.length()) <= maxCharsPerLine;
+      if (spaceLeftInLine) {
+        newLineBuilder.append(wordWithTrainlingSpace);
+        lineLength += wordWithTrainlingSpace.length();
+      } else {
+        newLineBuilder.deleteCharAt(newLineBuilder.length()-1);
+        newLineBuilder.append("\n");
+        newLineBuilder.append(wordWithTrainlingSpace);
+        lineLength = wordWithTrainlingSpace.length();
+      }
+    }
+
+    String wrappedLine = newLineBuilder.toString();
+    return wrappedLine.trim();
   }
 }
--- src/test/java/com/coremedia/codekata/wordwrap/SimpleWordWrapperTest.java
+++ src/test/java/com/coremedia/codekata/wordwrap/SimpleWordWrapperTest.java
@@ -13,9 +13,19 @@ public class SimpleWordWrapperTest {
   }
 
   @Test
-  public void noNeedToWrap() {
+  public void dontBreakShortLine() {
     String line = "Please don't wrap me!";
 
     Assert.assertEquals(line, wrapper.wrap(line, 100));
+  }
+
+  @Test
+  public void wrapOneTime() {
+    String line = "Please wrap me!";
+    String expected = "Please\nwrap me!";
+
+    String actual = wrapper.wrap(line, 10);
+
+    Assert.assertEquals(expected, actual);
   }
 }
