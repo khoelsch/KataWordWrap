66953c22b48cea6982edc548d7a715562bd9f711
Rainer Reich <rainer.reich@edgeway.de>
2012-01-08 17:57:15 +0100

    reformat

--- src/main/java/com/coremedia/codekata/wordwrap/LineWrapper.java
+++ src/main/java/com/coremedia/codekata/wordwrap/LineWrapper.java
@@ -1,12 +1,13 @@
 package com.coremedia.codekata.wordwrap;
 
 public interface LineWrapper {
+
   /**
    * Wraps a string after the specified number of characters.
-   *
+   * <p/>
    * Wrapping means simply adding "\n" at the appropriate places.
    *
-   * @param lineToWrap the string representing the line to wrap
+   * @param lineToWrap      the string representing the line to wrap
    * @param maxCharsPerLine the number of characters (columns) after a line break should be inserted
    * @return the wrapped line
    */
