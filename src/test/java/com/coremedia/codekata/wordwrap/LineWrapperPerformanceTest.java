7200c04c8175d910f467672b5d65f42327edc693
Rainer Reich <rainer.reich@edgeway.de>
2012-01-09 23:40:27 +0100

    minor refactoring

--- src/test/java/com/coremedia/codekata/wordwrap/LineWrapperPerformanceTest.java
+++ src/test/java/com/coremedia/codekata/wordwrap/LineWrapperPerformanceTest.java
@@ -4,6 +4,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,8 +15,9 @@ import java.util.concurrent.TimeUnit;
  */
 public final class LineWrapperPerformanceTest extends AbstractDataDrivenLineWrapperTest {
 
+  public static final String TEST_TXT = "ipsum.txt";
   public static final int MAX_CHARS_PER_LINE = 20;
-  private static final List<String> testText = new ArrayList<String>();
+  private static final List<String> listOfLines = new ArrayList<String>();
 
   public LineWrapperPerformanceTest(final LineWrapper wrapper) {
     super(wrapper);
@@ -23,28 +25,33 @@ public final class LineWrapperPerformanceTest extends AbstractDataDrivenLineWrap
 
   @BeforeClass
   public static void setUp() throws Exception {
-    final BufferedReader reader = new BufferedReader(new InputStreamReader(LineWrapperPerformanceTest.class.getClassLoader().getResourceAsStream("ipsum.txt")));
-
-    try {
-      String line;
-      while ((line = reader.readLine()) != null) {
-        testText.add(line);
-      }
-    } finally {
-      reader.close();
-    }
+    readTestText();
   }
 
   @Test
   public void testPerformance() {
     long startTime = System.nanoTime();
-    for (String line : testText) {
+    for (String line : listOfLines) {
       wrapper.wrap(line, MAX_CHARS_PER_LINE);
     }
     long finishTime = System.nanoTime();
 
     long duration = TimeUnit.NANOSECONDS.toMicros(finishTime - startTime);
     System.out.println(wrapper.getClass().getSimpleName() + " took " + duration + " microseconds");
+  }
+
+  private static void readTestText() throws IOException {
+    final BufferedReader reader = new BufferedReader(new InputStreamReader(
+            LineWrapperPerformanceTest.class.getClassLoader().getResourceAsStream(TEST_TXT)));
+
+    try {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        listOfLines.add(line);
+      }
+    } finally {
+      reader.close();
+    }
   }
 
 }
