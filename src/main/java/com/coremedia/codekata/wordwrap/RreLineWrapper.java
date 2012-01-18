9ee3812b5c6488354f29f7167350d046c11a4727
Rainer Reich <rainer.reich@edgeway.de>
2012-01-18 21:15:40 +0100

    make RreLineWrapperTest green

--- src/main/java/com/coremedia/codekata/wordwrap/RreLineWrapper.java
+++ src/main/java/com/coremedia/codekata/wordwrap/RreLineWrapper.java
@@ -7,19 +7,31 @@ public final class RreLineWrapper implements LineWrapper {
 
   @Override
   public String wrap(final String lineToWrap, final int maxCharsPerLine) {
+    if (maxCharsPerLine <= 0) {
+      return lineToWrap;
+    }
+
     final StringBuilder result = new StringBuilder();
     final StringBuilder rest = new StringBuilder(lineToWrap);
 
     while (rest.length() > maxCharsPerLine) {
       final int maxIndex = Math.min(rest.length(), maxCharsPerLine + 1);
       StringBuilder line = new StringBuilder(rest.substring(0, maxIndex));
-      final int lastBlankIndex = line.lastIndexOf(" ");
+      int lastBlankIndex = line.lastIndexOf(" ");
 
       // if the char at position 'maxCharsPerLine' + 1 is a blank,
       // we already have our line with exactly 'maxCharsPerLine' characters
       if (lastBlankIndex == line.length() - 1) {
         line.deleteCharAt(line.length() - 1);
-        result.append(line);
+      }
+      // else if no blank in line
+      else if (lastBlankIndex < 0) {
+        lastBlankIndex = rest.indexOf(" ");
+        if (lastBlankIndex >= 0) {
+          line.append(rest.substring(line.length(), lastBlankIndex));
+        } else {
+          break;
+        }
       }
       // else look after the last blank which will be the end of our line
       else {
